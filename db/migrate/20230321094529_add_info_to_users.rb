c1c20f99cdf0178e6c762a6574ee1ee54152d1dc
Luisa Ruiz <luisa.ruiz@ieseg.fr>
2023-03-21 10:49:50 +0100

    add info to users

---
+++ db/migrate/20230321094529_add_info_to_users.rb
@@ -0,0 +1,9 @@
+class AddInfoToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :email, :string
+    add_column :users, :phonenumber, :string
+    add_column :users, :age, :integer
+  end
+end
