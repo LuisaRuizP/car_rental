ec28857eb68840d0e3693501e1eba88ce032bb4a
Crysanne Yiannopoulos <crysanne.yiannopoulos@ieseg.fr>
2023-03-23 17:40:55 +0100

    Homepage Search Tab for Brand, Location, Passengers and Price

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,18 @@
 class CarsController < ApplicationController
   def index
     @cars = Car.all
+    if params[:brand].present?
+      @cars = @cars.where("brand ILIKE ?", "%#{params[:brand]}%")
+    end
+    if params[:location].present?
+      @cars = @cars.where("location ILIKE ?", "%#{params[:location]}%")
+    end
+    if params[:passengers].present?
+      @cars = @cars.where("passengers = ?", "#{params[:passengers]}")
+    end
+    if params[:price].present?
+      @cars = @cars.where("price = ?", "#{params[:price]}")
+    end
   end
 
 
